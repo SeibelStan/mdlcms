4cca14b2ed25705d960256b1222d55569247cf50
SeibelStan <seibel.stan@ya.ru>
2018-01-30 21:03:54 +0600

    Up

--- system/core.php
+++ system/core.php
@@ -315,3 +315,46 @@ function noimagize($data, $prop = 'image') {
     }
     return $data;
 }
+
+function curlGetAlong($urls) {
+    $mh = curl_multi_init();
+    $channels = [];
+
+    foreach($urls as $url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_multi_add_handle($mh, $ch);
+        $channels[$url] = $ch;
+    }
+
+    $active = 0;
+    do {
+        curl_multi_exec($mh, $active);
+    } while($active > 0);
+
+    $result = [];
+    foreach($channels as $channel) {
+        $result[] = curl_multi_getcontent($channel);
+        curl_multi_remove_handle($mh, $channel);
+    }
+
+    curl_multi_close($mh);
+    return $result;
+}
+
+function curlGetSeq($urls) {
+    $result = [];
+
+    foreach($urls as $url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result[] = curl_exec($ch);
+    }
+
+    curl_close($ch);
+    return $result;
+}
